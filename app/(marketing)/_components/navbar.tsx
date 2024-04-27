ccba98bdf0604e0f508dce863a778bc925aea073
swajp <stejskalmira@icloud.com>
2024-04-27 13:11:01 +0200

    removed dark mode

--- app/(marketing)/_components/navbar.tsx
+++ app/(marketing)/_components/navbar.tsx
@@ -31,7 +31,6 @@ export default function Navbar() {
             {item.label}
           </Link>
         ))}
-        <ModeToggle />
       </div>
     </nav>
   );
--- app/layout.tsx
+++ app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={cn("antialiased", font.className)}>
-        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+        <ThemeProvider attribute="class" defaultTheme="light">
           <div className="flex items-center ">
             <MaxWidthWrapper>{children}</MaxWidthWrapper>
           </div>
